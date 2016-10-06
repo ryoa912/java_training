75a7c455624231ef8da3f7ad08457f9f515058a3
Ryoh Aruga <ryo.a912@gmail.com>
2016-10-06 23:55:12 +0900

    [add]練習問題1.2を追加

---
+++ src/jpl/ch01/ex03/Fibonacci.java
@@ -0,0 +1,15 @@
+package jpl.ch01.ex03;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		System.out.println("[Fibonacci numbers up to 50]");
+		int lo = 1;
+		int hi = 1;
+		System.out.println(lo);
+		while(hi < 50) {
+			System.out.println(hi);
+			hi = lo + hi;
+			lo = hi - lo;
+		}
+	}
+}
