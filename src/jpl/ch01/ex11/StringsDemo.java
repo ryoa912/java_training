88ce52e02e83998bbf01149bf40071a55472e126
Ryoh Aruga <ryo.a912@gmail.com>
2016-10-12 22:11:20 +0900

    [add]練習問題1.11を追加

---
+++ src/jpl/ch01/ex11/StringsDemo.java
@@ -0,0 +1,10 @@
+package jpl.ch01.ex11;
+
+public class StringsDemo {
+	public static void main(String[] args) {
+		String myName = "Aruga";
+
+		myName = myName + " Ryoh";
+		System.out.println("Name = " + myName);
+	}
+}
---
+++ test/jpl/ch01/ex11/StringsDemoTest.java
@@ -0,0 +1,13 @@
+package jpl.ch01.ex11;
+
+import org.junit.Test;
+
+public class StringsDemoTest {
+
+	@Test
+	public void testMain() {
+		System.out.println("[目視確認]Name = Aruga Ryoh と表示されていること");
+		StringsDemo.main(null);
+	}
+
+}
