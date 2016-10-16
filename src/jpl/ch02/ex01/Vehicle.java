00fc59cfa2179cf92975990d2c1ddd85d1bdddd0
Ryoh Aruga <ryo.a912@gmail.com>
2016-10-16 17:06:31 +0900

    [add]練習問題2.1を追加

---
+++ src/jpl/ch02/ex01/Vehicle.java
@@ -0,0 +1,7 @@
+package jpl.ch02.ex01;
+
+public class Vehicle {
+	public int speed;
+	public int angle;
+	public String owner;
+}
---
+++ test/jpl/ch02/ex01/VehicleTest.java
@@ -0,0 +1,12 @@
+package jpl.ch02.ex01;
+
+import org.junit.Test;
+
+public class VehicleTest {
+
+	@Test
+	public void test() {
+		//操作を持たないのでテストなし
+	}
+
+}
