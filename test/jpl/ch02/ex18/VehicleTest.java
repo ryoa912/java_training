87de1f3900bd1364342e7df84d1c7025d436ce23
Ryoh Aruga <ryo.a912@gmail.com>
2016-11-03 23:01:18 +0900

    [add]練習問題2.18を追加

---
+++ test/jpl/ch02/ex18/VehicleTest.java
@@ -0,0 +1,42 @@
+package jpl.ch02.ex18;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import mockit.Deencapsulation;
+
+public class VehicleTest {
+
+	@Test
+	public void testMain_1() {
+		String[] input = new String[1];
+
+		input[0] = new String("Takashi");
+
+		Vehicle obj = new Vehicle();
+
+		obj.main(input);
+
+		String actual_owner = Deencapsulation.getField(obj, "owner");
+		int actual_id = Deencapsulation.getField(obj, "idNum");
+
+		assertEquals("Takashi", actual_owner);
+		assertEquals(1, actual_id);
+	}
+
+	@Test
+	public void testMain_2() {
+		String[] input = null;
+
+		Vehicle obj = new Vehicle();
+
+		obj.main(input);
+
+		String actual_owner = Deencapsulation.getField(obj, "owner");
+		int actual_id = Deencapsulation.getField(obj, "idNum");
+
+		assertEquals(null, actual_owner);
+		assertEquals(1, actual_id);
+	}
+}
