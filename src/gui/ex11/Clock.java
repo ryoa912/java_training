88034803bd83edfd521de0448310490291a07c06
Ryoh Aruga <ryo.a912@gmail.com>
2016-11-08 23:27:26 +0900

    [add][gui1-1]Clockクラスを作成、現在時刻をコンソールに表示するサンプルを作成

---
+++ src/gui/ex11/Clock.java
@@ -0,0 +1,11 @@
+package gui.ex11;
+
+import java.util.Date;
+
+public class Clock {
+
+	static public void main(String[] args) {
+		Date date = new Date();
+        System.out.println(date.toString());
+	}
+}
