315e8a5c285eeb5723598eea7f1e93a6cdd6506f
Ryoh Aruga <ryo.a912@gmail.com>
2016-11-08 23:07:42 +0900

    [add][gui1-1]閉じるボタンの実装 [add][gui1-1]簡単な図形と文字をpaintする処理を追加

--- src/gui/ex11/FrameTest.java
+++ src/gui/ex11/FrameTest.java
@@ -1,14 +1,34 @@
 package gui.ex11;
 
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class FrameTest extends Frame {
 
 	FrameTest() {
         super("FrameTest");
-        setSize(200, 100);
+        setSize(300, 250);
+        addWindowListener(new ClosingWindowListener());
     }
 
+    public void paint(Graphics g){
+       //(50,100)をベースラインの先頭にして文字列を描く
+       g.drawString("矩形と円を描く",50,100);
+
+       //左上の位置が(10,50)で縦150,横120ピクセルの矩形を描く
+       g.drawRect(10,50,150,120);
+
+       //左上の位置が(100,100)で縦100,横100の矩形に内接する円を描く
+       g.drawOval(100,100,100,100);
+   }
+
+	class ClosingWindowListener extends WindowAdapter {
+		public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		}
+	}
 
 	public static void main(String[] args){
 		FrameTest obj = new FrameTest();
