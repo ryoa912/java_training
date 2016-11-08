accd4ab72d7f595a168beed619ef90de941e8c72
Ryoh Aruga <ryo.a912@gmail.com>
2016-11-09 00:09:38 +0900

    [add][gui1-1]1秒おきにタイマ発火してコンソールにログを出すサンプルを作成

--- src/gui/ex11/Clock.java
+++ src/gui/ex11/Clock.java
@@ -1,11 +1,24 @@
 package gui.ex11;
 
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Clock {
-
+	public Clock() {
+		Timer timer = new Timer();
+	}
 	static public void main(String[] args) {
 		Date date = new Date();
         System.out.println(date.toString());
+
+        TimerTask task = new TimerTask() {
+            public void run() {
+                System.out.println("Wake Up");
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, 1000L, 1000L);
 	}
 }
