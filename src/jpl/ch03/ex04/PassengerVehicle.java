3413b74ccd06e6df288a61af50a173acedbd096e
Ryoh Aruga <ryo.a912@gmail.com>
2016-11-15 23:23:14 +0900

    [add]練習問題3.4を追加

---
+++ src/jpl/ch03/ex04/PassengerVehicle.java
@@ -0,0 +1,17 @@
+package jpl.ch03.ex04;
+
+public class PassengerVehicle {
+	public static final int SHEET_NUM = 4;
+	public int passengers;
+
+	public PassengerVehicle() {
+	}
+
+	public void setPassengers(int aPassengers) {
+		passengers = aPassengers >= SHEET_NUM ? SHEET_NUM : aPassengers;
+	}
+
+	public int getPassengers() {
+		return passengers;
+	}
+}
---
+++ src/jpl/ch03/ex04/Vehicle.java
@@ -0,0 +1,58 @@
+package jpl.ch03.ex04;
+
+public class Vehicle {
+	private int speed;
+	public int angle;
+	private String owner;
+	private int idNum;
+
+	private static int nextID = 0;
+	public enum EAngle {
+		TURN_RIGHT,
+		TURN_LEFT
+	}
+	public static final int ANGLE_RIGHT = 90;
+	public static final int ANGLE_LEFT = 270;
+
+	public Vehicle() {
+		idNum = ++nextID;
+	}
+
+	public final int getID() {
+		return idNum;
+	}
+
+	public void changeSpeed(int aSpeed) {
+		speed = aSpeed;
+	}
+
+	public final void stop() {
+		speed = 0;
+	}
+
+	public void turn(int aAngle) {
+		angle = aAngle;
+	}
+
+	public void turn(EAngle aAngle) {
+		switch (aAngle) {
+		case TURN_RIGHT:
+			angle = ANGLE_RIGHT;
+			break;
+		case TURN_LEFT:
+			angle = ANGLE_LEFT;
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void main (String[] args) {
+		if (args ==null) {
+			return;
+		}
+		if (args[0] != null) {
+			this.owner = args[0];
+		}
+	}
+}
