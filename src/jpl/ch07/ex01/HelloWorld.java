81b3272a5440fa09f1bd6963966f580e6a78de8a
Ryoh Aruga <ryo.a912@gmail.com>
2016-12-16 22:58:19 +0900

    [add]練習問題7.1を追加

---
+++ src/jpl/ch07/ex01/HelloWorld.java
@@ -0,0 +1,7 @@
+package jpl.ch07.ex01;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("H \u0b87 \u0b72 \u0b72 \u0b68, \u0b74 \u0b68 r \u0b72 \u0b37");
+	}
+}
