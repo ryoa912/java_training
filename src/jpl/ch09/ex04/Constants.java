0be0b7af197fbd44d1ea1a2653cd6b5c172c417d
Ryoh Aruga <ryo.a912@gmail.com>
2016-12-24 16:55:55 +0900

    [add]練習問題9.4を追加

---
+++ src/jpl/ch09/ex04/Constants.java
@@ -0,0 +1,45 @@
+package jpl.ch09.ex04;
+
+public class Constants {
+	public static final void printConstant1() {
+		System.out.println(3 << 2L - 1);
+	}
+	public static final void printConstant2() {
+		System.out.println((3L << 2) -1);
+	}
+	public static final void printConstant3() {
+		System.out.println(10 < 12 == 6 > 17);
+	}
+	public static final void printConstant4() {
+		System.out.println(10 << 12 == 6 >> 17);
+	}
+	public static final void printConstant5() {
+		System.out.println(13.5e-1 % Float.POSITIVE_INFINITY);
+	}
+	public static final void printConstant6() {
+		System.out.println(Float.POSITIVE_INFINITY + Double.NEGATIVE_INFINITY);
+	}
+	public static final void printConstant7() {
+		System.out.println(Double.POSITIVE_INFINITY - Float.NEGATIVE_INFINITY);
+	}
+	public static final void printConstant8() {
+		System.out.println(0.0 / -0.0 == -0.0 / 0.0);
+	}
+	public static final void printConstant9() {
+		System.out.println(Integer.MAX_VALUE + Integer.MIN_VALUE);
+	}
+	public static final void printConstant10() {
+		System.out.println(Long.MAX_VALUE + 5);
+	}
+	public static final void printConstant11() {
+		System.out.println((short) 5 * (byte) 10);
+	}
+	public static final void printConstant12() {
+		int i = 3;
+		System.out.println((i < 15 ? 1.72e3f : 0));
+	}
+	public static final void printConstant13() {
+		int i = 3;
+		System.out.println(i++ + i++ + --i);
+	}
+}
---
+++ test/jpl/ch09/ex04/ConstantsTest.java
@@ -0,0 +1,72 @@
+package jpl.ch09.ex04;
+
+import org.junit.Test;
+
+public class ConstantsTest {
+
+	@Test
+	public void testPrintConstant1() {
+		Constants.printConstant1();
+	}
+
+	@Test
+	public void testPrintConstant2() {
+		Constants.printConstant2();
+	}
+
+	@Test
+	public void testPrintConstant3() {
+		Constants.printConstant3();
+	}
+
+	@Test
+	public void testPrintConstant4() {
+		Constants.printConstant4();
+	}
+
+	@Test
+	public void testPrintConstant5() {
+		Constants.printConstant5();
+	}
+
+	@Test
+	public void testPrintConstant6() {
+		Constants.printConstant6();
+	}
+
+	@Test
+	public void testPrintConstant7() {
+		Constants.printConstant7();
+	}
+
+	@Test
+	public void testPrintConstant8() {
+		Constants.printConstant8();
+	}
+
+	@Test
+	public void testPrintConstant9() {
+		Constants.printConstant9();
+	}
+
+	@Test
+	public void testPrintConstant10() {
+		Constants.printConstant10();
+	}
+
+	@Test
+	public void testPrintConstant11() {
+		Constants.printConstant11();
+	}
+
+	@Test
+	public void testPrintConstant12() {
+		Constants.printConstant12();
+	}
+
+	@Test
+	public void testPrintConstant13() {
+		Constants.printConstant13();
+	}
+
+}
