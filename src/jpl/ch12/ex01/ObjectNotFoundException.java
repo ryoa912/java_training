5cd536878b61db2bc548a4c1ee1183275de501b2
Ryoh Aruga <ryo.a912@gmail.com>
2017-01-28 17:21:20 +0900

    [update]練習問題12.1を修正

--- src/jpl/ch12/ex01/ObjectNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package jpl.ch12.ex01;
-
-public class ObjectNotFoundException extends Exception {
-	public final Object object;
-	public ObjectNotFoundException(Object obj) {
-		super("Object not found");
-		object = obj;
-	}
-}
