4afe41dd661c7d8a8a846c79ef67fc08280ea9d7
Ryoh Aruga <ryo.a912@gmail.com>
2017-02-03 13:48:41 +0900

    [update]練習問題7.1を修正

--- src/jpl/ch07/ex01/HelloWorld.java
+++ src/jpl/ch07/ex01/HelloWorld.java
@@ -5,3 +5,6 @@ public class HelloWorld {
 		System.out.println("H \u0b87 \u0b72 \u0b72 \u0b68, \u0b74 \u0b68 r \u0b72 \u0b37");
 	}
 }
+
+//間違い。全てのソースコードを\XXXXで書かないとならない。
+//コンバーターを作って、toUnicodeで変換してやるのが手っ取り早い
