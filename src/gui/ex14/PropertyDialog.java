ef4656e96d97fbb77b1b10a7ed86ccc7babccd03
Ryoh Aruga <ryo.a912@gmail.com>
2017-02-14 21:26:27 +0900

    [update][gui1-4]プロパティダイアログをモーダル化

--- src/gui/ex14/PropertyDialog.java
+++ src/gui/ex14/PropertyDialog.java
@@ -11,13 +11,24 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 
-public class PropertyDialog extends Dialog implements ActionListener{
+public class PropertyDialog extends Dialog implements ActionListener {
 	GridBagLayout gbl = new GridBagLayout();
 
 	public PropertyDialog(Frame owner) {
-		super(owner);
-		this.setTitle("プロパティ");
+		super(owner, true);
+		//初期設定
+		setSize(600, 120);				//ダイアログサイズ
+		this.setTitle("プロパティ");	//タイトル
+		this.setResizable(false);		//リサイズ禁止
+
+		//閉じるボタンの設定
 		addWindowListener(new ClosingWindowListener(this));
+
+		Button but = (Button)this.add(new Button("OK"));
+		but.addActionListener(this);
+		but = (Button)this.add(new Button("キャンセル"));
+		but.addActionListener(this);
+
 		setLayout(gbl);
 		Button b1 = new Button();
 		addButton(b1, 0, 0, 1, 3);
@@ -36,6 +47,17 @@ public class PropertyDialog extends Dialog implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.out.println(e.getActionCommand());
+		switch (e.getActionCommand()) {
+		case "OK":
+			this.setVisible(false);
+			break;
+		case "キャンセル":
+			this.setVisible(false);
+			break;
+		default:
+			System.out.println("内部エラー");
+			break;
+		}
 	}
 	class ClosingWindowListener extends WindowAdapter {
 		Dialog f_frame;
