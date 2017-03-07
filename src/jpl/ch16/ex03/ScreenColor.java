2fe4ac97d1b8b0b0dfc14b73f33b414cf46e9793
Ryoh Aruga <ryo.a912@gmail.com>
2017-03-07 21:06:49 +0900

    [add]必要なファイルを追加

---
+++ src/jpl/ch16/ex03/ScreenColor.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright © 2017 Ryoh Aruga, All Rights Reserved.
+ */
+package jpl.ch16.ex03;
+
+public class ScreenColor {
+	private Object myColor; //変換される前の色
+
+	public ScreenColor(Object value) {
+		this.myColor = value;
+	}
+
+	public Object getValue() {
+		return this.myColor;
+	}
+}
