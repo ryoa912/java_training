e0c12edd709d73e38800918284271c92a605bb34
Ryoh Aruga <ryo.a912@gmail.com>
2017-03-21 22:23:56 +0900

    [add]練習問題17.3途中

---
+++ src/jpl/ch17/ex03/ResourceImpl.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright © 2017 Ryoh Aruga, All Rights Reserved.
+ */
+package jpl.ch17.ex03;
+
+public class ResourceImpl implements Resource {
+	int keyHash;
+	boolean needsRelease = false;
+
+	ResourceImpl(Object key) {
+		keyHash = System.identityHachCode(key);
+
+		//外部リソースの設定
+
+		needsRelease = true;
+	}
+
+	public void use(Object key, Object...objects args) {
+		if (System.identityHashCode(key) != keyHash)
+			throw new IllegalArgumentException("wrong key");
+
+		//リソースの使用
+	}
+
+	public synchronized void release() {
+		if (needsRelease) {
+			needsRelease = false;
+
+			//リソースの解放
+		}
+	}
+}
