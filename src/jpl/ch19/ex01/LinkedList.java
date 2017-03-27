22d0900dc962ace601f7ca487b36b52df2fcc5c3
Ryoh Aruga <ryo.a912@gmail.com>
2017-03-27 20:19:08 +0900

    [add]練習問題19.1を追加

---
+++ src/jpl/ch19/ex01/LinkedList.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright © 2017 Ryoh Aruga, All Rights Reserved.
+ */
+package jpl.ch19.ex01;
+
+public class LinkedList {
+	private Object obj;
+	private LinkedList next;
+
+	/**
+	 * Creates a new LinkedList node with the given object and previous LinkedList node.
+	 */
+	public LinkedList(Object object, LinkedList previous) {
+		obj = object;
+		if (null != previous)
+			previous.next = this;
+	}
+
+	/**
+	 * Returns the LinkedList node's object.
+	 */
+	public Object getObj() {
+		return obj;
+	}
+
+	/**
+	 * Returns the next LinkedList node.
+	 */
+	public LinkedList getNext() {
+		return next;
+	}
+
+	/**
+	 * Returns the LinkedList length.
+	 */
+	public int length() {
+		int ret = 1;
+
+		if (next != null) {
+			ret += next.length();;
+		}
+
+		return ret;
+	}
+}
