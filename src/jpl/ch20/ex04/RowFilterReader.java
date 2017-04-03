ec0032b206031178140fe712b460f6ff3f01f49a
Ryoh Aruga <ryo.a912@gmail.com>
2017-04-03 21:38:22 +0900

    [update]練習問題20.4を更新

--- src/jpl/ch20/ex04/RowFilterReader.java
+++ src/jpl/ch20/ex04/RowFilterReader.java
@@ -6,6 +6,7 @@ package jpl.ch20.ex04;
 import java.io.FilterReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
 
 public class RowFilterReader extends FilterReader {
 	public RowFilterReader(Reader in) {
@@ -13,15 +14,36 @@ public class RowFilterReader extends FilterReader {
 	}
 	public int read() throws IOException {
 		int c = super.read();
-		return c;		//TODO：1行貯まるまで待つ処理
+		return c;
 	}
 	public int read(char[] buf, int offset, int count) throws IOException {
 		int nread = super.read(buf, offset, count);
 		int last = offset + nread;
 		for (int i=offset; i< last; i++) {
-			buf[i] = buf[i];	//TODO：1行貯まるまで待つ処理
+			buf[i] = buf[i];
 		}
 		return nread;
 	}
+	public String readRow() throws IOException {
+		String str = "";
+		int c = 0;
+		while (true) {
+			c = this.read();
+			if (c == -1) {
+				return str;
+			}
+			str += (char)c;
+			if (str.contains("\\n")) {
+				return str;
+			}
+		}
 
+	}
+	public static void main(String[] args) throws IOException {
+		StringReader src = new StringReader(args[0]);
+		RowFilterReader f = new RowFilterReader(src);
+		String s = f.readRow();
+		System.out.print(s);
+		System.out.println();
+	}
 }
