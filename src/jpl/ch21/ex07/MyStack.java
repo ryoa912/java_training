665243f8822b3ea6d90f6725ad4d14a1711616ce
Ryoh Aruga <ryo.a912@gmail.com>
2017-05-05 23:20:37 +0900

    [fix]練習問題21.7を修正

--- src/jpl/ch21/ex07/MyStack.java
+++ src/jpl/ch21/ex07/MyStack.java
@@ -4,17 +4,19 @@
 package jpl.ch21.ex07;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
 
-public class MyStack extends ArrayList {
-	//ArrayListのサブクラスとすべきか、内部で使用すべきか
-	//→内部で使用すべき。（理由：サブクラスとすると、オーバーライドしていないメソッドについての動作保証も行わないとならないため。）
-	private ArrayList al = new ArrayList();
+public class MyStack<E> {
+	private List<E> list = new ArrayList<>();
 
-	public void push(Object obj) {
+	public void push(E element) {
+		list.add(element);
 	}
 
-	public Object pop() {
-		return null;
+	public E pop() {
+		if (list.isEmpty())
+			throw new EmptyStackException();
+		return list.remove(list.size() - 1);
 	}
-
 }
