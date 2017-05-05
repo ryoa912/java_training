dede75e41e72cfc4a0184d4f624086a788a17403
Ryoh Aruga <ryo.a912@gmail.com>
2017-05-05 23:10:13 +0900

    [fix]練習問題21.5を修正

--- src/jpl/ch21/ex05/ArrayBunchList.java
+++ src/jpl/ch21/ex05/ArrayBunchList.java
@@ -4,6 +4,9 @@
 package jpl.ch21.ex05;
 
 import java.util.AbstractList;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 public class ArrayBunchList<E> extends AbstractList<E> {
 	private final E[][] arrays;
@@ -43,5 +46,116 @@ public class ArrayBunchList<E> extends AbstractList<E> {
 			off += arrays[i].length;
 		}
 		throw new ArrayIndexOutOfBoundsException(index);
+	}
+
+	public Iterator<E> iterator() {
+		return new ABLIterator();
+	}
+
+	public ListIterator<E> listIterator() {
+		return new ABLListIterator();
+	}
+
+	private class ABLIterator implements Iterator<E> {
+		private int off;
+		private int array;
+		private int pos;
+
+		public ABLIterator() {
+			off = 0;
+			array = 0;
+			pos = 0;
+			for (array = 0; array < arrays.length; array++)
+				if (arrays[array].length > 0)
+					break;
+		}
+
+		public boolean hasNext() {
+			return off + pos < size();
+		}
+
+		public E next() {
+			if (!hasNext())
+				throw new NoSuchElementException();
+			E ret = arrays[array][pos++];
+			while (pos >= arrays[array].length) {
+				off += arrays[array++].length;
+				pos = 0;
+				if (array >= arrays.length)
+					break;
+			}
+			return ret;
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	private class ABLListIterator implements ListIterator<E> {
+		private int off;
+		private int array;
+		private int pos;
+
+		public ABLListIterator() {
+			off = 0;
+			array = 0;
+			pos = 0;
+			for (array = 0; array < arrays.length; array++)
+				if (arrays[array].length > 0)
+					break;
+		}
+
+		public boolean hasNext() {
+			return off + pos < size();
+		}
+
+		public E next() {
+			if (!hasNext())
+				throw new NoSuchElementException();
+			E ret = arrays[array][pos++];
+			while (pos >= arrays[array].length) {
+				off += arrays[array++].length;
+				pos = 0;
+				if (array >= arrays.length)
+					break;
+			}
+			return ret;
+		}
+
+		public boolean hasPrevious() {
+			return pos != 0;
+		}
+
+		public E previous() {
+			if (!hasPrevious())
+				throw new NoSuchElementException();
+			E ret = arrays[array][--pos];
+			if (pos < 0) {
+				off -= arrays[array--].length;
+				pos = arrays[array].length;
+			}
+			return ret;
+		}
+
+		public int nextIndex() {
+			return off + pos + 1;
+		}
+
+		public int previousIndex() {
+			return off + pos - 1;
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+		public void set(E e) {
+			throw new UnsupportedOperationException();
+		}
+
+		public void add(E e) {
+			throw new UnsupportedOperationException();
+		}
 	}
 }
