01ae95c789eac4a45189aac44504c542a3af1792
Ryoh Aruga <ryo.a912@gmail.com>
2017-05-05 21:51:27 +0900

    [add]練習問題22.12を追加

---
+++ src/jpl/ch22/ex12/Attributed.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright © 2017 Ryoh Aruga, All Rights Reserved.
+ */
+package jpl.ch22.ex12;
+
+public interface Attributed<V> {
+	void add(Attr<V> newAttr);
+	Attr<V> find(String attrName);
+	Attr<V> remove(String attrName);
+	java.util.Iterator<Attr<V>> attrs();
+}
