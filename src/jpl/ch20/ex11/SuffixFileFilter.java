a7d49bd0db77236b32039edd09e296273290bd4e
Ryoh Aruga <ryo.a912@gmail.com>
2017-06-02 11:22:43 +0900

    [fix]練習問題20.11を修正

--- src/jpl/ch20/ex11/SuffixFileFilter.java
+++ src/jpl/ch20/ex11/SuffixFileFilter.java
@@ -4,8 +4,9 @@
 package jpl.ch20.ex11;
 
 import java.io.File;
+import java.io.FilenameFilter;
 
-public class SuffixFileFilter {
+public class SuffixFileFilter implements FilenameFilter {
 	private final File dir;
 	private final String suffix;
 
@@ -19,11 +20,11 @@ public class SuffixFileFilter {
 	}
 
 	public String[] list() {
-		return dir.list();
+		return dir.list(this);
 	}
 
 	public static void main(String[] args) {
-		SuffixFileFilter filter = new SuffixFileFilter("/java_training", "java");
+		SuffixFileFilter filter = new SuffixFileFilter("/Applications", "java");
 		for (String list : filter.list())
 			System.out.println(list);
 	}
