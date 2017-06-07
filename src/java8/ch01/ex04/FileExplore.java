63aaf3fe5d128604ff8702e5323c09bef515c72e
Ryoh Aruga <ryo.a912@gmail.com>
2017-06-07 22:38:08 +0900

    [add][java8]練習問題1.4を追加

---
+++ src/java8/ch01/ex04/FileExplore.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright © 2017 Ryoh Aruga, All Rights Reserved.
+ */
+package java8.ch01.ex04;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileExplore {
+
+	public List<File> listFiles(String path) {
+		File[] list = new File(path).listFiles();
+		Arrays.sort(list, (File a, File b) -> {
+			if (a.isDirectory() && !b.isDirectory()) {
+				return -1;
+			} else if (!a.isDirectory() && b.isDirectory()) {
+				return 1;
+			} else {
+				return a.getName().compareTo(b.getName());
+			}
+		});
+		return Arrays.asList(list);
+}
+
+	public static void main(String[] args) {
+		FileExplore obj = new FileExplore();
+		List<File> l = new FileExplore().listFiles(new File(".").getAbsoluteFile().getParent());
+
+		for (int i=0; i<l.size(); i++) {
+			if (l.get(i).isDirectory())
+				System.out.print("[Dir ]");
+			else
+				System.out.print("[File]");
+			System.out.println(l.get(i).getPath());
+		}
+	}
+}
