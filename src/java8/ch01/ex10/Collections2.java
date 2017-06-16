46405b5410636584cd18ce7260c32c8f11e20efc
Ryoh Aruga <ryo.a912@gmail.com>
2017-06-16 21:35:16 +0900

    [add][java8]練習問題1.10を追加

---
+++ src/java8/ch01/ex10/Collections2.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright © 2017 Ryoh Aruga, All Rights Reserved.
+ */
+package java8.ch01.ex10;
+
+public class Collections2 {
+	//Set, List, Mapの3種類のインターフェースにマッピングするとしたらどうするということ？
+	//Collectionsのメソッド一覧：https://docs.oracle.com/javase/jp/8/docs/api/java/util/Collections.html
+	//全部で67メソッド。
+}
