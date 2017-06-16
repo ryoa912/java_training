85ac423445c892e1b89ecf830963b01825bc0573
Ryoh Aruga <ryo.a912@gmail.com>
2017-06-16 22:35:56 +0900

    [update]練習問題1.12を更新

---
+++ src/java8/ch01/ex12/Sample.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright © 2017 Ryoh Aruga, All Rights Reserved.
+ */
+package java8.ch01.ex12;
+
+public class Sample {
+	public static void main(String args) {
+		System.out.println("main");
+	}
+}
