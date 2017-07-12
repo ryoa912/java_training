4c66b4501a0640944f4f9d19033be0eb6fe6f7cd
Ryoh Aruga <ryo.a912@gmail.com>
2017-07-12 22:13:44 +0900

    [add][java8]練習問題2.13を追加、未完成

---
+++ src/java8/ch02/ex13/MyCounter.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright © 2017 Ryoh Aruga, All Rights Reserved.
+ */
+package java8.ch02.ex13;
+
+import java.util.stream.Stream;
+
+public class MyCounter {
+	public static int[] countShortWords(Stream<String> words, int threshold) {
+		return null;
+	}
+}
