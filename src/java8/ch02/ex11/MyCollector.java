de84995d5e9eb9cf5cab56894c132a87b8c18870
Ryoh Aruga <ryo.a912@gmail.com>
2017-07-12 22:11:28 +0900

    [add][java8]練習問題2.11を追加、未完成

---
+++ src/java8/ch02/ex11/MyCollector.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright © 2017 Ryoh Aruga, All Rights Reserved.
+ */
+package java8.ch02.ex11;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class MyCollector {
+	public static <T> ArrayList<T> collect(Stream<T> stream, int size) {
+		return null;
+	}
+}
