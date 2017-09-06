526f5168b6088600373495ce9050b951a34f173f
Ryoh Aruga <ryo.a912@gmail.com>
2017-09-06 21:57:50 +0900

    [update][add]VoiceroidPlayerを追加

---
+++ src/specialclock/components/VoiceroidPlayer.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright © 2017 Ryoh Aruga, All Rights Reserved.
+ */
+package specialclock.components;
+
+public class VoiceroidPlayer {
+	private String voiceroid_name = "TAMMY_EX";
+	private String wave_file_path = "src\\specialclock\\voice";
+	/*
+	public void setWavFilePath(String waveFilePath);	// VOICEROIDに音声データの保存ファイルパスを設定する
+	public void setTalkText(String talkText);			// VOICEROIDに発声するテキストを設定する
+	public void play();									// VOICEROIDを発声させる
+	public void save();									// VOICEROIDを発声させずに音声データを保存する
+	public void setAndPlay(String talkText);			// VOICEROIDに発声するテキストを設定し、発声させる
+	public void playAndSave();							// VOICEROIDを発声させ、音声データを保存する
+	public void setAndPlayAndSave(String talkText);		// VOICEROIDに発生するテキストを設定し、発声させ、音声データを保存する
+*/
+}
