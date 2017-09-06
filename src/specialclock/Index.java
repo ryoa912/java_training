a298c87ea3313014b4cb4090ce47a56619daf5f5
Ryoh Aruga <ryo.a912@gmail.com>
2017-09-06 21:44:14 +0900

    [add][specialclock]ボイスロイドの音声再生のプロと

--- src/specialclock/Index.java
+++ src/specialclock/Index.java
@@ -3,13 +3,26 @@
  */
 package specialclock;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.JFrame;
 
 import specialclock.containers.MainWindow;
 
 public class Index {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		createAndShowWindow();
+
+		Runtime rt = Runtime.getRuntime();
+
+		String path = new File(".").getAbsoluteFile().getParent();
+		System.out.println(path);
+
+		String[] str = new String[]{"src\\specialclock\\lib\\echoseika.exe",
+				"-cv", "TAMMY_EX", "おはようございます。涼しい朝ですね。"};
+
+		rt.exec(str);
 	}
 	private static void createAndShowWindow() {
 		JFrame mainWindow = new MainWindow();
